9400057d779dc1d779972a49d32205e96859a32b
Crowdbotics <team@crowdbotics.com>
2024-05-17 19:01:03 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "the-heirchivist-48009",
+  url: "https://the-heirchivist-48009.botics.co",
+  api: "https://the-heirchivist-48009.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
